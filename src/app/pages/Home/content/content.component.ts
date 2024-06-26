8e095a9829a62c04c018f88cc29e40eb81d02084
29jbarrera <29jbarrera@gmail.com>
2024-06-26 13:39:14 +0200

    Page content visual change about me

--- src/app/pages/Home/content/content.component.ts
+++ src/app/pages/Home/content/content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [CommonModule, DividerModule],
+  imports: [CommonModule, DividerModule, ButtonModule],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
