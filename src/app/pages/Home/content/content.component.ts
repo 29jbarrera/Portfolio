a6f5e27b962402996408fd8e3133790a966e493c
29jbarrera <29jbarrera@gmail.com>
2024-06-26 13:10:13 +0200

    Page Content visual changes

--- src/app/pages/Home/content/content.component.ts
+++ src/app/pages/Home/content/content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DividerModule],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
