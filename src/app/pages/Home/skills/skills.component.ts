b3a1a0197498dce1d4a32a87bff17f64b11a05b3
29jbarrera <29jbarrera@gmail.com>
2024-08-22 10:14:17 +0200

    visual changes

--- src/app/pages/Home/skills/skills.component.ts
+++ src/app/pages/Home/skills/skills.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressBarModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+  skillsValues = [20, 40, 60, 80]; // Valores para la primera columna
+  skillsValues2 = [25, 50, 75, 100];
+}
