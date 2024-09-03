6cbfe74aee969cfe2d2840a33d122ac1da62a971
29jbarrera <29jbarrera@gmail.com>
2024-09-03 18:48:37 +0200

    add form contact

---
+++ src/app/pages/Home/contact/contact.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [
+    CommonModule,
+    InputTextareaModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    HttpClientModule,
+  ],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss',
+})
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+
+      this.http.post('http://localhost:3000/send-email', formData).subscribe(
+        (response) => {
+          console.log('Correo enviado correctamente', response);
+          alert('Correo enviado correctamente');
+          this.contactForm.reset(); // Opcional: Limpiar el formulario después de enviar
+        },
+        (error) => {
+          console.error('Error al enviar el correo', error);
+          alert('Error al enviar el correo');
+        }
+      );
+    } else {
+      console.log('Formulario no válido');
+      alert('Por favor, complete el formulario correctamente.');
+    }
+  }
+}
--- src/app/pages/Home/home.page.ts
+++ src/app/pages/Home/home.page.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content/content.component';
 import { SkillsComponent } from './skills/skills.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ContentComponent, SkillsComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    ContentComponent,
+    SkillsComponent,
+    FooterComponent,
+    ContactComponent,
+  ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
