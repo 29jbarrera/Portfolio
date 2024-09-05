ad8d2a8a2c51a27f09921580ac958991f430596a
29jbarrera <29jbarrera@gmail.com>
2024-09-05 13:11:50 +0200

    add img projects and nav img

--- src/app/pages/projects/proyect/proyect.component.ts
+++ src/app/pages/projects/proyect/proyect.component.ts
@@ -1,18 +1,44 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleriaModule } from 'primeng/galleria';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-proyect',
   standalone: true,
-  imports: [CommonModule, GalleriaModule],
+  imports: [CommonModule, GalleriaModule, ButtonModule],
   templateUrl: './proyect.component.html',
   styleUrl: './proyect.component.scss',
 })
 export class ProyectComponent {
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input() imageUrl: string = '';
+  @Input() imageUrl: string[] = [];
   @Input() link: string = '';
   @Input() reverse: boolean = false;
+
+  currentIndex: number = 0;
+
+  ngOnInit() {
+    if (this.imageUrl.length === 0) {
+      console.warn('No images provided!');
+    }
+  }
+
+  showPrevious() {
+    if (this.imageUrl.length > 0) {
+      this.currentIndex =
+        (this.currentIndex - 1 + this.imageUrl.length) % this.imageUrl.length;
+    }
+  }
+
+  showNext() {
+    if (this.imageUrl.length > 0) {
+      this.currentIndex = (this.currentIndex + 1) % this.imageUrl.length;
+    }
+  }
+
+  getCurrentImageUrl(): string {
+    return this.imageUrl[this.currentIndex];
+  }
 }
