11ae01b1fecfb084c2efc1600031d552129f6827
29jbarrera <29jbarrera@gmail.com>
2024-09-08 10:52:51 +0200

    fix

--- src/app/pages/Home/skills/skills.component.ts
+++ src/app/pages/Home/skills/skills.component.ts
@@ -9,4 +9,17 @@ import { ProgressBarModule } from 'primeng/progressbar';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+  logos: string[] = [
+    '/assets/icons-skills/angular.svg',
+    '/assets/icons-skills/css.svg',
+    '/assets/icons-skills/Github_light.svg',
+    '/assets/icons-skills/gitlab.svg',
+    '/assets/icons-skills/html5.svg',
+    '/assets/icons-skills/javascript.svg',
+    '/assets/icons-skills/mongodb.svg',
+    '/assets/icons-skills/mysql.svg',
+    '/assets/icons-skills/PrimeFlexLogo.svg',
+    '/assets/icons-skills/typescript.svg',
+  ];
+}
