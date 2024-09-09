18e5889f587b4bd8fe4d5968bd3c78be85224195
29jbarrera <29jbarrera@gmail.com>
2024-09-09 08:59:12 +0200

    add page not found

--- src/app/pages/NotPageFound/NotPageFound.page.ts
+++ src/app/pages/NotPageFound/NotPageFound.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-not-page-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './NotPageFound.page.html',
-  styleUrl: './NotPageFound.page.scss'
+  styleUrl: './NotPageFound.page.scss',
 })
 export class NotPageFoundPage {}
