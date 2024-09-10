2b1519a7f10c188f3f8cdb2aa9a5f13b624abe9f
29jbarrera <29jbarrera@gmail.com>
2024-09-10 12:04:13 +0200

    add visual changes

--- src/app/pages/Home/skills/skills.component.ts
+++ src/app/pages/Home/skills/skills.component.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule, ProgressBarModule],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
-  logos: string[] = [
-    '/assets/icons-skills/angular.svg',
-    '/assets/icons-skills/css.svg',
-    '/assets/icons-skills/Github_light.svg',
-    '/assets/icons-skills/gitlab.svg',
-    '/assets/icons-skills/html5.svg',
-    '/assets/icons-skills/javascript.svg',
-    '/assets/icons-skills/mongodb.svg',
-    '/assets/icons-skills/mysql.svg',
-    '/assets/icons-skills/PrimeFlexLogo.svg',
-    '/assets/icons-skills/typescript.svg',
-  ];
-}
+export class SkillsComponent {}
