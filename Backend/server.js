348e0cafc59ed5d92ebc53a98c060e44a26758d6
29jbarrera <29jbarrera@gmail.com>
2024-09-11 10:18:08 +0200

    add icons

--- Backend/server.js
+++ Backend/server.js
@@ -2,30 +2,27 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
-// Crear una instancia de Express
 const app = express();
 
-// Middleware
-app.use(cors()); // Permite que el frontend (Angular) haga peticiones al backend
-app.use(bodyParser.json()); // Procesa los datos JSON enviados en las solicitudes
+app.use(cors());
+app.use(bodyParser.json());
 
-// Ruta POST para manejar el envío de correos
 app.post("/send-email", (req, res) => {
   const { name, email, subject, message } = req.body;
 
-  // Configuración de Nodemailer (usando Gmail como ejemplo)
   const transporter = nodemailer.createTransport({
-    service: "Gmail", // Servicio de correo que estés usando (Gmail en este caso)
+    service: "Gmail",
     auth: {
-      user: "29jbarrera@gmail.com", // Tu correo (necesitas activar la autenticación de aplicaciones en Gmail)
-      pass: "klfk jlut yuhs jvqm", // Tu contraseña o token de la app de Gmail
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: email, // El correo del usuario que llena el formulario
-    to: "29jbarrera@gmail.com", // El correo al que quieres enviar los mensajes
+    from: email,
+    to: process.env.EMAIL_USERNAME,
     subject: `¡¡Nuevo mensaje en mi Portfolio!!`,
     html: `
       <h2><strong>Asunto:</strong> ${subject}</h2>
@@ -35,19 +32,16 @@ app.post("/send-email", (req, res) => {
       <h2>${message}</h2>
     `,
   };
-  // Enviar el correo
+
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error("Error al enviar el correo:", error);
       res.status(500).json("Error al enviar el correo");
     } else {
-      console.log("Correo enviado: " + info.response);
       res.status(200).json("Correo enviado correctamente");
     }
   });
 });
 
-// Iniciar el servidor en el puerto 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
